538144570a339822adce353208e9be4af4008188
王诗扬 <wangshiyang02@meituan.com>
2018-07-15 01:42:31 +0800

    publish log: branch:master;msg:test
    f88c0eb4ac866c4a8d53d3782569bd0f842575f9514984ed4d09221c726df9af|4095b15186a7a5950edce9f36020572b3b6693eabb5c43d949d134c113d6991ff66ab12e28447e1c08216efc8ac81851a74aa34ca3452affc6f1a6cf6892ee0e3331333533333331333533393330333133353331333433333333376333353632333336313333333636343333333536333336363433363333333233363334333836333334333433333331363333363636333633353633333033323633333033363330333633303336363336353061

--- examples-webpack/webpack.prod.conf.js
+++ examples-webpack/webpack.prod.conf.js
@@ -52,10 +52,8 @@ var webpackConfig = merge(baseWebpackConfig, {
     // you can customize output by editing /index.html
     // see https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
-      filename: process.env.NODE_ENV === 'testing'
-        ? 'index.html'
-        : config.build.index,
-      template: 'index.html',
+      filename: config.build.index,
+      template: 'indexh5.html',
       inject: true,
       minify: {
         removeComments: true,
